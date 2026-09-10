fef04c042e7adbc9e200814300e11391cea4e9c7
joyrahaASC <joy.raha@ascendion.com>
2026-09-10 11:00:55 -0500

    Sync test script tests/web/scripts/test_suite_01_scripts.py

--- tests/web/scripts/test_suite_01_scripts.py
+++ tests/web/scripts/test_suite_01_scripts.py
@@ -5,8 +5,6 @@ from playwright.sync_api import expect
 
 from pageObjects.web.login_page import LoginPage
 from pageObjects.web.dashboard_page import DashboardPage
-from pageObjects.web.footer_navigation import FooterNavigation
-from pageObjects.web.support_portal import SupportPortal
 from pageObjects.web.common_scenario import CommonScenario
 
 def load_test_data():
@@ -15,27 +13,17 @@ def load_test_data():
         return json.load(f)
 
 def test_suite_01_scripts(page, request):
-    # TestRail ID: C102
+    # TestRail ID: C101
     test_data = load_test_data()
     scenario = CommonScenario(page, request)
     
     loginPage = LoginPage(page, scenario)
     dashboardPage = DashboardPage(page, scenario)
-    footerNavigation = FooterNavigation(page, scenario)
-    supportPortal = SupportPortal(page, scenario)
     
     loginPage.go_to()
     loginPage.valid_login(test_data["username"], test_data["password"])
-    
-    footerNavigation.scroll_to_footer()
-    footerNavigation.click_contact_us_link()
-    
-    supportPortal.navigate_to_support_portal()
-    supportPortal.input_subject()
-    supportPortal.input_message()
-    supportPortal.click_submit_button()
-    
-    supportPortal.locate_success_confirmation_message()
-    dashboardPage.wait_for_success_message()
-    dashboardPage.assert_success_message_visible()
-    supportPortal.assert_confirmation_text()
+
+    dashboardPage.locate_profile_name_element()
+    dashboardPage.assert_user_profile_name_visible()
+    dashboardPage.search_product_add_cart("Zara Coat 3")
+    dashboardPage.navigate_to_cart()
